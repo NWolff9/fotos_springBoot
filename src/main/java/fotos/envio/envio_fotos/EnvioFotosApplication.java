99360ee08626a7fc1e4320229ae190c54a67fb95
Nathã Wolff <natha.wolff9@gmail.com>
2023-06-10 01:55:51 -0300

    enviar fotos

---
+++ src/main/java/fotos/envio/envio_fotos/EnvioFotosApplication.java
@@ -0,0 +1,13 @@
+package fotos.envio.envio_fotos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnvioFotosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnvioFotosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fotos/envio/envio_fotos/EnvioFotosApplicationTests.java
@@ -0,0 +1,13 @@
+package fotos.envio.envio_fotos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnvioFotosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
